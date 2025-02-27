a9b390136c10324ba80d1f46f0a32d130346fe6e
MAYURIRAMNATHSHINDE <mayurishinde24304@gmail.com>
2025-02-28 00:15:13 +0530

    render deployment

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
